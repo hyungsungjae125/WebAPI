2028b1413ddc99257a36639e887e59b555ffc820
형성재 <tldwo125@naver.com>
2019-02-08 13:54:09 +0900

    디비정보

--- Solution/WebApplication/Modules/DataBase.cs
+++ Solution/WebApplication/Modules/DataBase.cs
@@ -20,7 +20,7 @@ namespace WebApplication.Modules
         private bool GetConnection()
         {
             conn = new MySqlConnection();
-            conn.ConnectionString = string.Format("server={0};user={1};password={2};database={3};port={4}", "gdc3.gudi.kr", "root", "1234", "gudi", "30002");
+            conn.ConnectionString = string.Format("server={0};user={1};password={2};database={3};port={4}", "192.168.3.155", "root", "1234", "gudi", "3306");
             try
             {
                 conn.Open();
